e6fc0e8a30b51e424774d4174ce9270272f8caa5
owenbarford <owen_barford@hotmail.com>
2018-02-28 14:01:47 +0000

    Second commit

---
+++ app_server/controllers/agents.js
@@ -0,0 +1,14 @@
+/* GET 'home' page */ 
+const homelist = function(req, res){
+    res.render('agents-list', { title: 'Home' }); 
+}; 
+ 
+/* GET 'Agent info' page */
+const agentInfo = function(req, res){
+    res.render('index', { title: 'Agent info' }); 
+}; 
+
+module.exports = {   
+    homelist,
+    agentInfo    
+}; 
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
-/* GET home page. */
-router.get('/', ctrlMain.index);
+const ctrlAgents = require('../controllers/agents');
+const ctrlOthers = require('../controllers/others');
+
+/* Agents pages. */
+router.get('/', ctrlAgents.homelist);
+router.get('/agent', ctrlAgents.agentInfo);
+
+/* Other pages. */
+router.get('/about', ctrlOthers.about);
+
 module.exports = router;
