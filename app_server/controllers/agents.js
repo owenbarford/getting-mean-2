1e2844e76edcab4f8b91b40392aabb4fbf0ff4bb
owenbarford <owen_barford@hotmail.com>
2018-03-09 11:45:52 +0000

    API bug fix

--- app_server/controllers/agents.js
+++ app_server/controllers/agents.js
@@ -3,7 +3,7 @@ const apiOptions = {
     server : 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = 'https://aqueous-peak-91782.herokuapp.com/';
+    apiOptions.server = 'https://aqueous-peak-91782.herokuapp.com';
 }
 
 // PUBLIC EXPOSED METHODS
