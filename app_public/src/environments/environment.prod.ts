2cefd3ae5f77770b418886cc0b29d90bd78c547e
owenbarford <owen_barford@hotmail.com>
2018-07-04 14:49:58 +0100

    try https URIs

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   NVM_CLIENT_ID: 'wwxcdqiapdx',
   NVM_DATA_URL: 'https://cloud11.contact-world.net/v0',
-  API_URL: 'http://suppappuk.com/api',
-  NVM_API_URL: 'http://suppappapiuk.com/api'
+  API_URL: 'https://www.suppappuk.com/api',
+  NVM_API_URL: 'https://www.suppappapiuk.com/api'
 };
